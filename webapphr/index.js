f308235429c85b34bffcabade166a5c853a48608
GianlucaPCGI <gianluca.piccirillo@cgi.com>
2022-08-17 15:58:47 -0400

    create employee

--- webapphr/index.js
+++ webapphr/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require("dotenv").config();
+require('dotenv').config();
 
 console.log(process.env.DB_NAME);
+console.log(process.env.DB_PASSWORD);
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
