b713b6448311457d9d5e9638a7e0b17ef165931d
GianlucaPCGI <gianluca.piccirillo@cgi.com>
2022-08-17 12:50:04 -0400

    sql

--- webapphr/index.js
+++ webapphr/index.js
@@ -28,7 +28,7 @@ app.get('/test', (req, res) => {
 
     let query = db.query(sql, (err, result) => {
         if (err) throw err;
-        console.log(result[1].title);
+        console.log(result[1]);
         res.json({ message: "success" });
         res.end();
     });
