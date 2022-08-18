2a417c4be6c460fe512f7a523be365ee3701ef3b
GianlucaPCGI <gianluca.piccirillo@cgi.com>
2022-08-18 13:25:36 -0400

    webhook

--- serverad/index.js
+++ serverad/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { exec } = require('child_process');
-const { log } = require('console');
 
 
 const app = express();
@@ -17,11 +16,19 @@ app.get('/', (req, res) => {
     res.end();
 });
 
-app.post('/test', (req, res) => {
+app.post('/test', async (req, res) => {
     if (req.body.hook == true) {
         console.log("success and true");
-        res.status(207)
-        res.end();
+        const resp = await automate();
+
+        if (resp.status) {
+            res.status(207);
+            res.end();
+        }else {
+            console.log(`error: ${resp.error}\nscript #: ${resp.scriptNumber}`);
+            res.status(400)
+            res.end();
+        }
     }else {
         console.log("success and not true");
         res.status(400)
@@ -29,20 +36,25 @@ app.post('/test', (req, res) => {
     }
 });
 
-app.get('/script', async function(req, res) {
+async function automate() {
     const scripts = ['ls -a', 'ls', 'echo "hello world"'];
-
+    
     for (let i = 0; i < scripts.length; i++) {   
-
         try {
             await useScript(scripts[i]);
         }catch (e) {
             console.log(e);
+            return {
+                error: e,
+                scriptNumber: i + 1,
+                status: false,
+            }
         }
     }
-         
-    res.end();
-});
+    return {
+        status: true
+    }
+}
 
 function useScript(cmd) {
     return new Promise(function(resolve, reject) {
